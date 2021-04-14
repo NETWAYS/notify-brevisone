a68fc6d2aeb1e492492305a859a4fbd7237e913b
rincewind <rincewind@vulgrim.de>
2021-04-14 15:56:49 +0200

    Put config in a nice struct

--- notify-brevisOne.go
+++ notify-brevisOne.go
@@ -14,6 +14,24 @@ import (
 const readme = `Notifications via a Brevis.One gateway.
 Sends SMS or rings at a given number`
 
+type Config struct {
+	gateway	string
+	target	string
+	targetType string
+	ring bool
+	username string
+	password string
+	skipVerify bool
+	checkState string
+	checkOutput string
+	notificationAuthor string
+	hostName string
+	serviceName string
+	comment string
+	date string
+	notificationType string
+}
+
 func main() {
 	defer check.CatchPanic()
 
@@ -23,81 +41,77 @@ func main() {
 	plugin.Timeout = 30
 	plugin.Version = "0.1"
 
-	gateway := plugin.FlagSet.StringP("gatewayIP", "g", "", "IP/Address of the Brevis.one gateway (required)")
-	target := plugin.FlagSet.StringP("target", "", "", "Contact name (Group or contact) or phone number (required)")
+	config := &Config{}
+
+	// BrevisOne related configuration
+	plugin.FlagSet.StringVarP(&config.gateway, "gateway", "g", "", "IP/Address of the Brevis.one gateway (required)")
+	plugin.FlagSet.StringVarP(&config.target, "target", "", "", "Contact name (Group or contact) or phone number (required)")
 	// TODO:Detect target type automatically?
-	targetType := plugin.FlagSet.StringP("targetType", "", "number", "Type of the contact, may be one of: number, contact or contactgroup")
-	ring := plugin.FlagSet.BoolP("ring", "r", false, "Ring mode (optional, if not set, send SMS)")
+	plugin.FlagSet.StringVarP(&config.targetType, "targetType", "", "number", "Type of the contact, may be one of: number, contact or contactgroup")
+	plugin.FlagSet.BoolVarP(&config.ring, "ring", "r", false, "Ring mode (optional, if not set, send SMS)")
+	plugin.FlagSet.StringVarP(&config.username, "username", "u", "", "API user name (required)")
+	plugin.FlagSet.StringVarP(&config.password, "password", "p", "", "API user password (required)")
+	plugin.FlagSet.BoolVarP(&config.skipVerify, "skipTlsVerify", "", false, "Skip verification of the TLS certificates (is needed for the default self signed certificate of the Brevis.One device")
 
-	username := plugin.FlagSet.StringP("username", "u", "", "API user name (required)")
-	password := plugin.FlagSet.StringP("password", "p", "", "API user password (required)")
-	skipVerify := plugin.FlagSet.BoolP("skipTlsVerify", "", false, "Skip verification of the TLS certificates (is needed for the default self signed certificate of the Brevis.One device")
-
-	checkState := plugin.FlagSet.IntP("checkresult", "s", -1, "Return code of the host/service check (required)")
-	checkOutput := plugin.FlagSet.StringP("output", "o", "", "Output of the host/service check (required)")
-	notificationAuthor := plugin.FlagSet.StringP("notificationAuthor", "a", "", "Author of the notification (optional)")
-	hostName := plugin.FlagSet.StringP("host", "", "", "Name of the host object (required)")
+	// Message configuration
+	plugin.FlagSet.StringVar(&config.checkState, "checkresult", "s",  "Return code of the host/service check (required)")
+	plugin.FlagSet.StringVarP(&config.checkOutput, "output", "o", "", "Output of the host/service check (required)")
+	plugin.FlagSet.StringVarP(&config.notificationAuthor, "notificationAuthor", "a", "", "Author of the notification (optional)")
+	plugin.FlagSet.StringVarP(&config.hostName, "host", "", "", "Name of the host object (required)")
 	//hostDisplayName := plugin.FlagSet.StringP("hostDisplay", "", "", "Display name of the host object (optional)")
-	serviceName := plugin.FlagSet.StringP("service", "", "", "Name of the service object (required for service notifications)")
+	plugin.FlagSet.StringVarP(&config.serviceName, "service", "", "", "Name of the service object (required for service notifications)")
 	//serviceDisplayName := plugin.FlagSet.StringP("serviceDisplay", "", "", "Display name of the service object (optional)")
-	comment := plugin.FlagSet.StringP("comment", "", "", "Notification comment (optional)")
-	date := plugin.FlagSet.StringP("date", "", "", "Notification date")
-
-	notificationType := plugin.FlagSet.StringP("type", "", "", "Notification type (e.g. Problem, Recovery, etc.")
+	plugin.FlagSet.StringVarP(&config.comment, "comment", "", "", "Notification comment (optional)")
+	plugin.FlagSet.StringVarP(&config.date, "date", "", "", "Notification date")
+	plugin.FlagSet.StringVarP(&config.notificationType, "type", "", "", "Notification type (e.g. Problem, Recovery, etc.")
 
 	// Parsing the arguments
 	plugin.ParseArguments()
 
-	if *username == "" {
+	if config.username == "" {
 		check.ExitError(errors.New("Username not set"))
 	}
 
-	if *password == "" {
+	if config.password == "" {
 		check.ExitError(errors.New("Password not set"))
 	}
 
-	if *gateway == "" {
+	if config.gateway == "" {
 		check.ExitError(errors.New("Gateway IP/Address not set"))
 	}
 
-	if *target == "" {
+	if config.target == "" {
 		check.ExitError(errors.New("Contact not set"))
 	}
 
-	if *targetType != "number" && *targetType != "contact" && *targetType != "contactgroup" {
+	if config.targetType != "number" && config.targetType != "contact" && config.targetType != "contactgroup" {
 		check.ExitError(errors.New("Not a valid targetType"))
 	}
 
-	if *hostName == "" {
+	if config.hostName == "" {
 		check.ExitError(errors.New("Host name must be set"))
 	}
 
-	if *checkState == -1 {
+	if config.checkState == "" {
 		check.ExitError(errors.New("Check result (return code) must be set"))
 	}
 
 	msg := ""
-	if *date != "" {
-		msg += *date + "/"
+	if config.date != "" {
+		msg += config.date + "/"
 	}
-	if *notificationType != "" {
-		msg += *notificationType + ": "
+	if config.notificationType != "" {
+		msg += config.notificationType + ": "
 	}
 
-	if *serviceName != "" {
+	if config.serviceName != "" {
 		// This is a service notification
-		msg += fmt.Sprintf("Srvc:%s on %s is %s", *serviceName, *hostName, check.StatusText(*checkState))
+		msg += fmt.Sprintf("Srvc:%s @ %s - %s", config.serviceName, config.hostName, config.checkState)
 	} else {
-		longState := ""
-		if *checkState == 0 {
-			longState = "OK"
-		} else {
-			longState = "DOWN"
-		}
-		msg += fmt.Sprintf("Hst:%s is %s", *hostName, longState)
+		msg += fmt.Sprintf("Hst:%s - %s", config.hostName, config.checkState)
 	}
 
-	msg += "\n" + *checkOutput
+	msg += " - " + config.checkOutput
 
 	remainingSymbols := 160 - len(msg)
 	if remainingSymbols < 0 {
@@ -105,27 +119,27 @@ func main() {
 		msg = msg[0:159]
 	}
 
-	if *comment != "" && remainingSymbols >= (len(*comment)+1) {
-		msg += "\n" + *comment
+	if config.comment != "" && remainingSymbols >= (len(config.comment)+1) {
+		msg += "\n" + config.comment
 
-		if *notificationAuthor != "" && ((159 - len(msg)) < len(*notificationAuthor)) {
-			msg += "\n" + *notificationAuthor
+		if config.notificationAuthor != "" && ((159 - len(msg)) < len(config.notificationAuthor)) {
+			msg += "\n" + config.notificationAuthor
 		}
 	}
 
 	var tlsConf tls.Config
-	if *skipVerify {
+	if config.skipVerify {
 		tlsConf.InsecureSkipVerify = true
 	}
 
 	client := &http.Client{Transport: &http.Transport{TLSClientConfig: &tlsConf}}
 
-	var baseUrl string = "https://" + *gateway + "/api/"
+	var baseUrl string = "https://" + config.gateway + "/api/"
 
 	// ========================
 	// Get authentication token
 	// ========================
-	signinCreds := []byte("{\"username\": \"" + *username + "\",\"password\": \"" + *password + "\"}")
+	signinCreds := []byte("{\"username\": \"" + config.username + "\",\"password\": \"" + config.password + "\"}")
 	req, err := http.NewRequest("POST", baseUrl+"signin", bytes.NewBuffer(signinCreds))
 	if err != nil {
 		check.ExitError(err)
@@ -166,7 +180,7 @@ func main() {
 	// Send message
 	// ============
 
-	recipients := `[{"to":"` + *target + `","target":"` + *targetType + `"}]`
+	recipients := `[{"to":"` + config.target + `","target":"` + config.targetType + `"}]`
 	text := `"text":"` + msg + `"`
 	provider := `"provider":"sms"`
 	providerType := `"type":"default"`
@@ -200,7 +214,7 @@ func main() {
 		check.ExitError(errors.New("Could not send message"))
 	}
 
-	if *ring {
+	if config.ring {
 		// Additional request to ring
 		providerType = `"type":"ring"`
 		messageBody = fmt.Sprintf(`{"recipients":%s,%s,%s,%s}`, recipients, text, provider, providerType)
