64eb228525644c347f0e8332a07e90e680031103
Markus Frosch <markus.frosch@netways.de>
2021-05-03 12:18:00 +0200

    test: Add integration test with settings from env

---
+++ integration_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestConfig_Run_Integration(t *testing.T) {
+	var (
+		gateway  = os.Getenv("BREVIS_INTEGRATION_GATEWAY")
+		username = os.Getenv("BREVIS_INTEGRATION_USERNAME")
+		password = os.Getenv("BREVIS_INTEGRATION_PASSWORD")
+		target   = os.Getenv("BREVIS_INTEGRATION_TARGET")
+		ring     = os.Getenv("BREVIS_INTEGRATION_RING") != ""
+	)
+
+	if gateway == "" || username == "" || password == "" || target == "" {
+		t.Skip("You need to set BREVIS_INTEGRATION_* environment for integration tests")
+		return
+	}
+
+	c := &Config{
+		gateway:          gateway,
+		target:           target,
+		targetType:       "number",
+		ring:             ring,
+		username:         username,
+		password:         password,
+		insecure:         true,
+		checkState:       "WARNING",
+		checkOutput:      "test notification for notify-brevisone",
+		hostName:         "brevis.one",
+		serviceName:      "notify",
+		author:           "icingaadmin",
+		comment:          "comment",
+		date:             "",
+		notificationType: "CUSTOM",
+	}
+
+	assert.NoError(t, c.Run())
+}
