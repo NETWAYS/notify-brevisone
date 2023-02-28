5138bb31107b2805c3a17e187d2f0226d2389ae5
Markus Opolka <markus@martialblog.de>
2023-02-28 16:29:03 +0100

    Extend tests

--- api_test.go
+++ api_test.go
@@ -21,6 +21,32 @@ func TestApiClient_Login(t *testing.T) {
 	assert.Equal(t, "abc123", ac.Token)
 }
 
+func TestApiClient_LoginErr(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("POST", "https://brevisone.local/api/signin",
+		httpmock.NewStringResponder(401, `{}`))
+
+	ac := NewApiClient("brevisone.local")
+
+	err := ac.Login("admin", "password")
+	assert.Error(t, err)
+}
+
+func TestApiClient_UnmarshalErr(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("POST", "https://brevisone.local/api/signin",
+		httpmock.NewStringResponder(200, `{`))
+
+	ac := NewApiClient("brevisone.local")
+
+	err := ac.Login("admin", "password")
+	assert.Error(t, err)
+}
+
 func TestApiClient_DoRequest(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
@@ -34,4 +60,11 @@ func TestApiClient_DoRequest(t *testing.T) {
 	response, err := ac.DoRequest("test", nil)
 	assert.NoError(t, err)
 	assert.Equal(t, `{"test":true}`, string(response))
+}
+
+func TestApiClient_DoRequestErr(t *testing.T) {
+	ac := NewApiClient("local")
+
+	_, err := ac.DoRequest("test", nil)
+	assert.Error(t, err)
 }
